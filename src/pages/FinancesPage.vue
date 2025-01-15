9e5b6c83ce588b8e1ce0874aaf15071aabe085c3
ebagabe-dev <ebagabe.2025@gmail.com>
2025-01-15 15:33:41 -0300

    feat(finances): create btn to toggle about cards and lists

---
+++ src/pages/FinancesPage.vue
@@ -0,0 +1,36 @@
+<template>
+  <q-page padding>
+    <div class="row q-col-gutter-md">
+      <div class="col-12">
+        <h1 class="text-h4">Finanças</h1>
+      </div>
+
+      <div class="col-12">
+        <q-btn-toggle
+          v-model="viewMode"
+          flat
+          stretch
+          toggle-color="primary"
+          :options="[
+            { label: 'Cards', value: 'cards' },
+            { label: 'Lista', value: 'list' },
+          ]"
+        />
+      </div>
+
+      <div class="col-12">
+        <component :is="currentView" />
+      </div>
+    </div>
+  </q-page>
+</template>
+
+<script setup lang="ts">
+import { ref, computed } from 'vue'
+import FinanceCards from 'src/components/FinanceCards.vue'
+import FinanceList from 'src/components/FinanceList.vue'
+
+const viewMode = ref('cards')
+
+const currentView = computed(() => (viewMode.value === 'cards' ? FinanceCards : FinanceList))
+</script>
